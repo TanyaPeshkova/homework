37cb37cd04e84e6da81bdbfb4377a08d5c53b293
TanyaPeshkova <peshkova-2002.01@mail.ru>
2021-05-11 12:44:30 +0700

    update

--- js/general.js
+++
@@ -1,5 +0,0 @@
-function upDate(){
-  document.getElementById("galereya").style.backgroundImage = "url("../image/hk.jpg")";
-  document.getElementById("galereya").innerHTML = "";
-  document.getElementById("galereya").style.backgroundColor = "#CCCCFF";
-	}
